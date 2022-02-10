3fdfef5ac212a5cfc5903ef68b80778e0a11585e
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-02-10 12:23:54 -0800

    Clean up files in examples

--- examples/local_lcls/server.py
+++ examples/local_lcls/server.py
@@ -7,13 +7,17 @@ from lume_epics_impact.utils import load_configuration
 import logging
 
 if __name__ == "__main__":
-    with open("examples/local_lcls/variables.yml", "r") as f:
+  #  with open("examples/local_lcls/variables.yml", "r") as f:
+
+    with open("/Users/jgarra/sandbox/lume-epics-impact/examples/examples/local_lcls/variables.yml", "r") as f:
         input_variables, output_variables = variables_from_yaml(f)
 
-    with open("examples/local_lcls/epics_config.yml", "r") as f:
+  #  with open("examples/local_lcls/epics_config.yml", "r") as f:
+    with open("/Users/jgarra/sandbox/lume-epics-impact/examples/local_lcls/epics_config.yml", "r") as f:
+
         epics_config = config_from_yaml(f)
 
-    config = load_configuration("examples/local_lcls/sdf_lcls.toml")
+    config = load_configuration("/Users/jgarra/sandbox/lume-epics-impact/examples/local_lcls/local_lcls.toml")
 
     server = Server(
         ImpactModel,
